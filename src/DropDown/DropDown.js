b4167befe00a7d0216a874c5c30bf1c838c25151
bleo <bleo@mit.edu>
2022-09-24 16:37:13 -0400

    initial commit

---
+++ src/DropDown/DropDown.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './DropDown.css';
+
+const DropDown = () => {
+
+    return(
+        <div className='drop-down-container'>
+            <div className='drop-down-selected'>
+            Select Option
+            </div>
+            <div className='drop-down-menu'>
+                I am the option
+            </div>
+        </div>)
+}
+
+export default DropDown
